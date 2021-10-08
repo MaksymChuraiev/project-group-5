375162d5fec9737954e8f8211bd4b9dfa20ffc81
MaksymChuraiev <mchuraev1988@gmail.com>
2021-10-08 13:08:23 +0300

    create partials

---
+++ src/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.header__button-open'),
+    closeMenuBtn: document.querySelector('.mob-menu__button-close'),
+    menu: document.querySelector('.mob-menu'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
