fd25b4cbf685d0b8abe824c23264a56dfa2bccc3
MaksymChuraiev <87321814+MaksymChuraiev@users.noreply.github.com>
2021-10-09 13:50:06 +0000

    Deploying to gh-pages from @ MaksymChuraiev/project-group-5@f93d53573255a4a8d9fc4668324ce8691bbec00b 🚀

--- src.b5c16e7b.js
+++ src.b5c16e7b.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-group-5/src.3e638804.js.map
+//# sourceMappingURL=/project-group-5/src.b5c16e7b.js.map
